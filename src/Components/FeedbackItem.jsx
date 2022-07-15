51e5c622e5d215f60e4fe6fca44945991b4daa30
Thomas <tholmes59@gmail.com>
2022-07-15 09:16:59 -0700

    add delete buttonn and onclick functionality to pass to App.js to delete feedback

--- src/Components/FeedbackItem.jsx
+++ src/Components/FeedbackItem.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Card from './shared/Card'
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa'
 
+function FeedbackItem({item, handleDelete}) {
 
-function FeedbackItem({item}) {
-    
   return (
     <Card >
       <div className="num-display">{item.rating}</div>
+      <button className='close' onClick={() => handleDelete(item.id)}>
+        <FaTimes color='purple'/>
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   )
