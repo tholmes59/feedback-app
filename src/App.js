c3f5b88a3c673a7254ed00457b9b3814f6714ea4
Thomas <tholmes59@gmail.com>
2022-07-15 09:20:34 -0700

    add function to delete feedback on handle click event and import feedback stats componenet

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import { useState } from "react"
 import Header from "./Components/Header"
 import FeedbackList from "./Components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
+import FeedbackStats from "./Components/FeedbackStats"
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+       if(window.confirm('Are you sure')){
+           setFeedback(feedback.filter((item) => item.id !== id))
+
+       }
+    }
     return (
         <>
         <Header text='Feedback'/>
         <div className="container">
-            <FeedbackList feedback={feedback}/>
+            <FeedbackStats feedback={feedback}/>
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
         </div>
         
         </>
