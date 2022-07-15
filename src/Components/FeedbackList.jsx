224a96ecdbae24223bdeafa1543cce3d9e378c13
Thomas <tholmes59@gmail.com>
2022-07-15 09:19:16 -0700

    pass props for feedback delete handleclick event and add proptypes for feedback props

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 import FeedbackItem from './FeedbackItem'
 import PropTypes from 'prop-types'
 
-function FeedbackList({feedback}) {
+function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0) {
         return <p>No feedback yet</p>
     }
   return (
     <div className='feedback-list'>
       {feedback.map((item) => (
-          <FeedbackItem key={item.id} item={item} />
+          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
       ))}
     </div>
   )
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        text: PropTypes.string.isRequired,
-        rating: PropTypes.number.isRequired,
-    })
+    feedback: PropTypes.arrayOf(
+      PropTypes.shape({
+          id: PropTypes.number.isRequired,
+          text: PropTypes.string.isRequired,
+          rating: PropTypes.number.isRequired,
+      })
+    )
 }
 
 export default FeedbackList
