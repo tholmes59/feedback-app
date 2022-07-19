d76a05eb93e511339a1341765bf999777af7bd04
Thomas <tholmes59@gmail.com>
2022-07-19 09:24:08 -0700

    create about page component and add react router link back to homepage

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../Components/shared/Card'
+import {Link} from 'react-router-dom'
+
+function AboutPage() {
+  return (
+    <Card className='about'>
+      <h1>About This Project</h1>
+      <p>This is a React App to leave feedback for a product or service</p>
+      <p>Version 1.0.0</p>
+      <p><Link to="/">Back to home</Link></p>
+    </Card >
+  )
+}
+
+export default AboutPage
