ab25b1b6ac33592a13900c105cec59c32c7ff4bd
Thomas <tholmes59@gmail.com>
2022-07-19 12:48:43 -0700

    import feedback provider file in context folder, wrap components on Feedback Provider, and remove passed props to feedback stats and feedback list components

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackForm from "./Components/FeedbackForm"
 import { v4 as uuidv4} from 'uuid'
 import AboutPage from "./pages/AboutPage"
 import AboutIconLink from "./Components/AboutIconLink"
+import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -25,23 +26,25 @@ function App() {
     }
 
     return (
-        <Router>
-        <Header text='Feedback'/>
-        <div className="container">
-            <Routes>
-                <Route exact path='/' element={
-                    <>
-                        <FeedbackForm handleAdd={addFeedback}/>
-                        <FeedbackStats feedback={feedback}/>
-                        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-                    </>
-                }>
-                </Route>
-                <Route path='/about' element={<AboutPage/>}/>
-            </Routes>
-            <AboutIconLink/>
-        </div>
-        </Router>
+        <FeedbackProvider>
+            <Router>
+            <Header text='Feedback'/>
+            <div className="container">
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback}/>
+                            <FeedbackStats/>
+                            <FeedbackList handleDelete={deleteFeedback}/>
+                        </>
+                    }>
+                    </Route>
+                    <Route path='/about' element={<AboutPage/>}/>
+                </Routes>
+                <AboutIconLink/>
+            </div>
+            </Router>
+        </FeedbackProvider>
     )
 }
 
