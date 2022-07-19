607e57c159f2144558a8014dcd5308107175e30a
Thomas <tholmes59@gmail.com>
2022-07-19 12:51:12 -0700

    import usecontext and feedback from feedback context file and remove proptypes from feedback list component

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import FeedbackItem from './FeedbackItem'
-import PropTypes from 'prop-types'
 import {motion, AnimatePresence} from 'framer-motion'
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({feedback, handleDelete}) {
-    if(!feedback || feedback.length === 0) {
-        return <p>No feedback yet</p>
-    }
+function FeedbackList({handleDelete}) {
+  const {feedback} = useContext(FeedbackContext)
   
-    return (
-        <div className='feedback-list'>
-          <AnimatePresence>
-              {feedback.map((item) => (
-                <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-                  <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
-                </motion.div>
-              ))}
-          </AnimatePresence>
-        </div>
-      )
+  if(!feedback || feedback.length === 0) {
+      return <p>No feedback yet</p>
+  }
+
+  return (
+      <div className='feedback-list'>
+        <AnimatePresence>
+            {feedback.map((item) => (
+              <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+              </motion.div>
+            ))}
+        </AnimatePresence>
+      </div>
+    )
   
   // return (
   //   <div className='feedback-list'>
@@ -27,16 +30,6 @@ function FeedbackList({feedback, handleDelete}) {
   //     ))}
   //   </div>
   // )
-}
-
-FeedbackList.propTypes = {
-    feedback: PropTypes.arrayOf(
-      PropTypes.shape({
-          id: PropTypes.number.isRequired,
-          text: PropTypes.string.isRequired,
-          rating: PropTypes.number.isRequired,
-      })
-    )
 }
 
 export default FeedbackList
