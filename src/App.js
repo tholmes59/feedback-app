8eb5e146e983c4071956ebd4173983cc31468a48
Thomas <tholmes59@gmail.com>
2022-07-19 09:26:07 -0700

    add react router, import About page, and add routes for home page and about page. Also add link to home page to route to about page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Header from "./Components/Header"
 import FeedbackList from "./Components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./Components/FeedbackStats"
 import FeedbackForm from "./Components/FeedbackForm"
 import { v4 as uuidv4} from 'uuid'
+import AboutPage from "./pages/AboutPage"
+import AboutIconLink from "./Components/AboutIconLink"
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -22,15 +25,23 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
         <Header text='Feedback'/>
         <div className="container">
-            <FeedbackForm handleAdd={addFeedback}/>
-            <FeedbackStats feedback={feedback}/>
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+            <Routes>
+                <Route exact path='/' element={
+                    <>
+                        <FeedbackForm handleAdd={addFeedback}/>
+                        <FeedbackStats feedback={feedback}/>
+                        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+                    </>
+                }>
+                </Route>
+                <Route path='/about' element={<AboutPage/>}/>
+            </Routes>
+            <AboutIconLink/>
         </div>
-        
-        </>
+        </Router>
     )
 }
 
