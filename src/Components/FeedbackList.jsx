5c866eea9d10e949fd372545b0e77f0327ebe6f3
Thomas <tholmes59@gmail.com>
2022-07-20 09:04:44 -0700

    remove prop padding of handle delete through feedback list to feedback item due to the use of use context hooks

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -4,7 +4,7 @@ import {motion, AnimatePresence} from 'framer-motion'
 import {useContext} from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackList({handleDelete}) {
+function FeedbackList() {
   const {feedback} = useContext(FeedbackContext)
   
   if(!feedback || feedback.length === 0) {
@@ -16,7 +16,7 @@ function FeedbackList({handleDelete}) {
         <AnimatePresence>
             {feedback.map((item) => (
               <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
-                <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+                <FeedbackItem key={item.id} item={item}/>
               </motion.div>
             ))}
         </AnimatePresence>
