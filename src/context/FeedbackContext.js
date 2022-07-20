b18e181018c1017213e60da343097546c4792d90
Thomas <tholmes59@gmail.com>
2022-07-20 09:00:50 -0700

    add feedback edit state, move functions for deleteing, updating, adding and editing feedback to feedback context file

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { v4 as uuidv4} from 'uuid'
 
 const FeedbackContext = createContext()
 
@@ -6,13 +7,59 @@ export const FeedbackProvider = ({children}) => {
     const [feedback, setFeedback] = useState([
         {
             id: 1,
-            text: 'This item is from context',
+            text: 'This is feedback item 1',
             rating: 10
+
+        },
+        {
+            id: 2,
+            text: 'This is feedback item 2',
+            rating: 9
+
+        },
+        {
+            id: 3,
+            text: 'This is feedback item 3',
+            rating: 7
 
         }
     ])
+
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure')){
+            setFeedback(feedback.filter((item) => item.id !== id))
+ 
+        }
+     }
+
+     const updateFeedback = (id, updItem) => {
+         setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+     } 
+
+     const addFeedback = (newFeedack) => {
+        newFeedack.id = uuidv4()
+        setFeedback([newFeedack, ...feedback])
+    }
+
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true
+        })
+    }
+
     return <FeedbackContext.Provider value={{
-        feedback
+        feedback,
+        deleteFeedback,
+        addFeedback,
+        editFeedback,
+        feedbackEdit,
+        updateFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
