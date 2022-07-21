995fc23e12a3f867437b55eb6258482758a1821d
Thomas <tholmes59@gmail.com>
2022-07-21 14:14:09 -0700

    import isloading prop via context and spinner component and add fucntionality to conditional to display spinner while data loads

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -3,16 +3,17 @@ import FeedbackItem from './FeedbackItem'
 import {motion, AnimatePresence} from 'framer-motion'
 import {useContext} from 'react'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList() {
-  const {feedback} = useContext(FeedbackContext)
+  const {feedback, isLoading} = useContext(FeedbackContext)
   
-  if(!feedback || feedback.length === 0) {
+  if(!isLoading && (!feedback || feedback.length === 0)) {
       return <p>No feedback yet</p>
   }
 
-  return (
-      <div className='feedback-list'>
+  return isLoading ? <Spinner/> : (
+    <div className='feedback-list'>
         <AnimatePresence>
             {feedback.map((item) => (
               <motion.div key={item.id} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
@@ -21,7 +22,7 @@ function FeedbackList() {
             ))}
         </AnimatePresence>
       </div>
-    )
+  )
   
   // return (
   //   <div className='feedback-list'>
